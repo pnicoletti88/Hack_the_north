1f3c70b9aba9673c4d986c9330c7ad41984eed39
Phil <pnicolet@uwo.ca>
2019-09-14 20:58:25 -0400

    Webhooks done

--- server/src/Routes/freshbookHooks.js
+++ server/src/Routes/freshbookHooks.js
@@ -1,14 +1,57 @@
 const express = require('express');
+const axios = require('axios');
 
 const Router = express.Router();
 const createhook = require('./webhooks');
 
-Router.post('/invoice', (req, res) => {
+const urlHook = (account,id) => `https://api.freshbooks.com/events/account/${account}/events/callbacks/${id}`;
+const urlVoice = (account,invoice) => `https://api.freshbooks.com/accounting/account/${account}/invoices/invoices/${invoice}`
+const urlExp = (account,invoice) => `https://api.freshbooks.com/accounting/account/${account}/expenses/expenses/${invoice}`
 
+
+Router.post('/invoice', async (req, res) => {
+    res.send();
+    const invoiceToken = req.body.object_id;
+    const accountID = req.body.account_id;
+    console.log(urlVoice(accountID, invoiceToken));
+    const result = await axios.get(urlVoice(accountID, invoiceToken), {
+        headers:{
+            Authorization: `Bearer ${process.env.BEARER}`
+        }
+    })
+    const invoice = result.data.response.result.invoice;
+    
 });
 
-Router.post('/expense', (req, res) => {
-
+Router.post('/expense', async (req, res) => {
+    res.send();
+    const expenseToken = req.body.object_id;
+    const accountID = req.body.account_id;
+    const result = await axios.get(urlExp(accountID, expenseToken), {
+        headers:{
+            Authorization: `Bearer ${process.env.BEARER}`
+        }
+    })
+    const expense = result.data.response.result.expense;
+    
 });
 
 module.exports = Router;
+
+/*
+const { verifier, object_id } = req.body;
+    console.log(url("n7nxNe", object_id));
+    const result = await axios.put(url("n7nxNe", object_id), {
+        'callback': {
+            'callback_id': object_id,
+            'verifier': verifier
+        }
+    }, {
+        headers:{
+            Authorization: "Bearer aa694c1478b49f7499e5ebc9957afddf67a7c047b15f70652a6642f70ca38fde"
+        }
+    });
+    console.log(result);
+
+
+*/
